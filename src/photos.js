a2c3689272249ef8267515a06ba207898efe0c61
ops <ops@MacBook-Air-ops.local>
2022-06-05 18:20:37 +0300

    first commit

---
+++ src/photos.js
@@ -0,0 +1,873 @@
+export const photos = [
+  {
+    src: "./photos/1.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/2.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/3.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/4.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/5.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/6.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/7.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/8.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/9.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/10.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/11.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/12.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/13.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/14.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/15.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/16.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/17.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/18.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/19.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/20.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/21.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/22.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/23.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/24.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/25.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/26.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/27.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/28.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/29.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/30.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/31.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/32.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/33.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/34.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/35.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/36.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/37.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/38.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/39.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/40.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/41.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/42.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/43.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/44.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/45.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/46.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/47.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/48.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/49.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/50.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/51.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/52.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/53.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/54.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/55.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/56.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/57.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/58.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/59.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/60.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/61.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/62.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/63.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/64.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/65.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/66.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/67.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/68.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/69.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/70.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/71.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/72.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/73.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/74.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/75.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/76.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/77.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/78.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/79.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/80.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/81.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/82.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/83.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/84.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/85.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/86.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/87.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/88.jpg",
+    width: 3,
+    height: 4.5,
+  },
+
+  {
+    src: "./photos/90.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/91.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/92.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/93.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/94.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/95.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/96.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/97.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/98.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/99.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/100.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/101.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/102.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/103.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/104.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/105.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/106.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/107.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/108.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/109.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/110.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/111.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/112.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/113.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/114.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/115.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/116.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/117.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/118.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/119.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/120.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/121.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/122.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/123.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/124.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/125.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/126.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/127.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/128.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/129.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/130.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/131.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/132.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/133.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/134.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/135.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/136.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/137.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/138.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/139.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/140.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/141.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/142.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/143.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/144.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/145.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/146.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/147.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/148.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/149.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/150.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/151.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/152.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/153.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/154.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/155.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/156.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/157.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/158.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/159.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/160.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/161.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/162.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/163.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/164.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/165.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/166.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/167.jpg",
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: "./photos/168.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/169.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/170.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/171.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/172.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/173.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/174.jpg",
+    width: 3,
+    height: 4.5,
+  },
+  {
+    src: "./photos/175.jpg",
+    width: 3,
+    height: 4.5,
+  },
+];
